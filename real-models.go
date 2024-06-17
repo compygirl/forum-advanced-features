3c6ce0ec3e02cadc5200f2d70271ec4f7db830eb
aigera <aigera@aigeras-MacBook-Pro.local>
2024-06-17 21:06:32 +0500

    removed the sensitive google and github api keys

--- real-models.go
+++ real-models.go
@@ -5,6 +5,20 @@ import (
 	"time"
 )
 
+const (
+	GoogleAuthURL      = "https://accounts.google.com/o/oauth2/auth"                                // const URL
+	GoogleClientID     = "722031461724-dnvp1cl4hngcs1kgt0a2qi9j86a3dr1n.apps.googleusercontent.com" // my google account
+	GoogleRedirectURL  = "https://localhost:8080/auth/google/callback"                              // callback endpoint
+	GoogleClientSecret = "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"                                      // my google account
+)
+
+const (
+	GitHubAuthURL      = "https://github.com/login/oauth/authorize"
+	GitHubClientID     = "7204d1f96b4db7e5d453"
+	GitHubRedirectURL  = "https://localhost:8080/auth/github/callback"
+	GitHubClientSecret = "2a4621170475143853a9752bf405fb1d2f781051"
+)
+
 type GitHubResponseToken struct {
 	AccessToken string `json:"access_token"`
 	TokenID     string `json:"id_token"`
